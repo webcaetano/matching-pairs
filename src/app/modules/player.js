45ae74f9a2a10834e411edcb047793f05d59323d
nick <nicholasmordecai@gmail.com>
2015-12-10 11:05:29 +0000

    added custom functions to game state

---
+++ src/app/modules/player.js
@@ -0,0 +1,12 @@
+var utils = require('./utils');
+var _ = require('lodash');
+var Phaser = require('phaser');
+
+
+module.exports = function(game,scope){
+
+	function userInput() {
+		console.log('foobar');
+	}
+
+}
--- src/app/states/game.js
+++ src/app/states/game.js
@@ -16,6 +16,10 @@ module.exports = function(game, scope) {
                 this.rows = 3;
                 this.cols = 4;
                 this.grid = new Array();
+                this.user = {
+                    firstSelection: null,
+                    secondSelection: null
+                };
             },
 
             preload() {
@@ -23,6 +27,9 @@ module.exports = function(game, scope) {
             },
 
             create() {
+                // include modules
+                var player = require('../modules/player.js')(game, scope);
+
                 // create background image
                 var background = game.add.image(0, 0, 'bg');
                 background.height = game.height;
@@ -40,22 +47,33 @@ module.exports = function(game, scope) {
                 var positions = new Array();
                 // start on used true so first loop forces a new instance
                 var index = 0;
-                var numbers = {number: null, used: true};
+                var numbers = {
+                    number: null,
+                    used: true
+                };
                 for (var x = 0; x <= this.cols; x++) {
                     for (var y = 0; y <= this.rows; y++) {
-                    	if (numbers.used === true) {
-                    		numbers.number = game.rnd.integerInRange(0,3);
-                    		numbers.used = false;
-                    		positions.push({frame:numbers.number, index: index, pair: index});
-                    	} else {
-                    		numbers.used = true;
-                    		positions.push({frame:numbers.number, index: index, pair: index -1});
-                    	}
-                    	index++;
+                        if (numbers.used === true) {
+                            numbers.number = game.rnd.integerInRange(0, 3);
+                            numbers.used = false;
+                            positions.push({
+                                frame: numbers.number,
+                                index: index,
+                                pair: index
+                            });
+                        } else {
+                            numbers.used = true;
+                            positions.push({
+                                frame: numbers.number,
+                                index: index,
+                                pair: index - 1
+                            });
+                        }
+                        index++;
                     }
                 }
 
-               utils.shuffleArray(positions);
+                utils.shuffleArray(positions);
 
                 for (var x = 0; x <= this.cols; x++) {
                     for (var y = 0; y <= this.rows; y++) {
@@ -70,18 +88,18 @@ module.exports = function(game, scope) {
 
                 // add artwork
                 for (var i in this.grid) {
-                	var spriteName = "box" + positions[i].index;
-                	this.grid[spriteName] = game.add.sprite(this.grid[i].posX, this.grid[i].posY, 'gameImages', 4);
-                	this.grid[spriteName].anchor.set(0.5);
+                    var spriteName = "box" + positions[i].index;
+                    this.grid[spriteName] = game.add.sprite(this.grid[i].posX, this.grid[i].posY, 'gameImages', 4);
+                    this.grid[spriteName].anchor.set(0.5);
                     this.grid[spriteName].originalFrame = positions[i].frame;
-                	this.grid[spriteName].customID = positions[i].index;
-                	this.grid[spriteName].inputEnabled = true;
-					this.grid[spriteName].input.useHandCursor = true;
-					this.grid[spriteName].events.onInputUp.add(function() {
-                        userInput(this);
-					},this.grid[spriteName]);
+                    this.grid[spriteName].customID = positions[i].index;
+                    this.grid[spriteName].inputEnabled = true;
+                    this.grid[spriteName].input.useHandCursor = true;
+                    var that = this;
+                    this.grid[spriteName].events.onInputUp.add(function() {
+                        that.userInput(this);
+                    }, this.grid[spriteName], this);
                 }
-                console.log(this.grid);
             },
 
             update() {
@@ -89,7 +107,7 @@ module.exports = function(game, scope) {
             },
 
             userInput(obj) {
-
+                obj.frame = obj.originalFrame;
             }
     }
 }
